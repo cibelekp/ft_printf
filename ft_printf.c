6abe91a0cef6e5ab50ea53d5620cb7f6102b168f
gitpck <cibele.kp@outlook.com>
2022-12-04 20:16:25 +0000

    ft_printf

---
+++ ft_printf.c
@@ -0,0 +1,21 @@
+
+/*
+STEPS:
+1)structure for defining different formats
+2)main function: 
+ -check if not %
+ -if % -> evaluate which kind of variable is being received
+3)(aux) functions for each format
+*/
+
+/*
+INSTRUCTIONS:
+-prototyped similarly to printf
+-won’t do the buffer management in the printf function
+-manage following conversions: sSpdDioOuUxXcC
+-manage %%
+-manage flags #0-+space
+-manage min field-width
+-manage precision
+-manage flags hh, h, 1, 11, j, z
+*/
---
+++ makefile
@@ -0,0 +1,2 @@
+# Your must submit a Makefile which will compile a libftprintf.a. This lib will be
+# linked to our testing main to give you your results
